07df1d72220e00b80d40686a33df99228d4fc3e2
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-06-29 15:26:36 -0700

    Completed assignment

---
+++ lib/student.rb
@@ -0,0 +1,10 @@
+# Super class to chatty_student
+class Student
+    def hello
+        puts "Hey there! I'm so excited to learn stuff."
+    end
+
+    def raise_hand
+        puts "Pick me!"
+    end
+end
